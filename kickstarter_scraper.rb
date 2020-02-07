deb0b366d02ebdaaa49c8fca3ede09ceae41abd2
Ninibeth Teixeira <ninibethreina@gmail.com>
2020-02-07 01:47:58 +0000

    Automatically backed up by Learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -3,17 +3,26 @@ require 'pry'
 
 def create_project_hash
   html= File.read('fixtures/kickstarter.html')
-  
   kickstarter= Nokogiri::HTML(html)
-  binding.pry 
-end
+ 
+  projects={ }
+  
+  kickstarter.css("li.project.grid_4").each do |project|
+   title= projects.css("h2.bbcard_name strong a").text 
+   projects[title.to_sym]= { 
 
-#projects: kickstarter.css("li.project.grid_4")
-#title: project.css("h2.bbcard_name strong a").text 
-#image link: project.css("div.project-thumbnail a img").attribute("src").value 
-#description: project.css("p.bbcard_blurb").text 
-#location: project.css("ul.project-meta span.location-name").text 
-#percent_funded: project.css("ul.project-stats li.first.funded strong").text.gsub("%", "").to_i 
+
+:title => project.css("h2.bbcard_name strong a").text, 
+:image_link => project.css("div.project-thumbnail a img").attribute("src").value, 
+:description => project.css("p.bbcard_blurb").text, 
+:location => project.css("ul.project-meta span.location-name").text, 
+:percent_funded => project.css("ul.project-stats li.first.funded strong").text.gsub("%", "").to_i 
+}
+end 
+projects 
+end 
+
+
 
 
 create_project_hash
